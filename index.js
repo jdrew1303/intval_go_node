dda4682e2ec46b1b420d3acc91d7ff9cb7f19143
mmcwilliams <hi@mmcwilliams.com>
2018-08-27 17:47:48 -0400

    Plenty of work on the node concept app

--- index.js
+++ index.js
@@ -41,41 +41,85 @@ async function dependencies () {
 	}
 }
 
+function delay (ms) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+}
+
 function args () {
 	INTVAL = process.argv[process.argv.length - 2];
 	VIDEO = process.argv[process.argv.length - 1];
 	if (INTVAL.indexOf('/index.js') !== -1 || INTVAL.indexOf('/bin/node') !== -1) {
 		exit('Requires 2 arguments', 1)
 	}
+
 }
 
-async function ffmpeg_frame () {
+async function sequence (info) {
+
+}
+
+async function ffmpeg_frame (time) {
 	//ffmpeg -i ${VIDEO} -ss 00:00:07.000 -vframes 1 thumb.jpg
 
 	//ffmpeg -i ${VIDEO} -compression_algo raw -pix_fmt rgb24 output.tiff
 }
 
+async function display_frame (frame) {
+	//timeout 3 eog --fullscreen ${frame}
+}
+
 
 async function ffprobe () {
 	let cmd = `ffprobe -v quiet -print_format json -show_format -show_streams "${VIDEO}"`
+	let exists;
+	try {
+		exists = await fs.exists(VIDEO);
+	} catch (err) {
+		return exit(err, 5);
+	}
+	if (!exists) {
+		return exit(`File ${VIDEO} does not exist`, 6);
+	}
+	return asyncExec(cmd);
+}
+
+async function ffprobe_frames () {
+	let cmd = `ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 ${VIDEO}`;
 	return asyncExec(cmd);
 }
 
 async function main () {
 	let info;
+	let frames;
 	//check dependencies (ffmpeg, eog, preview)
-	await dependencies();
+	try {
+		await dependencies();
+	} catch (err) {
+		//will exit process on error
+	}
 	//parse arguments
 	args();
-	//for loop exporting video
+	
 	try {
 		info = await ffprobe();
 	} catch (err) {
 		return exit(err, 2);
 	}
-	if (info) {
+	try {
+		frames = await ffprobe_frames();
+	} catch (err) {
+		return exit(err, 3);
+	}
+	if (info && frames) {
 		info = JSON.parse(info);
-		console.dir(info);
+		frames = parseInt(frames);
+		//console.dir(info);
+		console.log(frames);
+		//for loop exporting video
+	} else {
+		return exit('')
 	}
 }
 
